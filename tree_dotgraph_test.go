6b570038f56fa6ce355d0b6d70dfe64426524d60
Athas <wanghao@zkme.xyz>
2024-10-17 14:27:19 +0800

    feat: add greenfield-iavl v0.20.1

---
+++ tree_dotgraph_test.go
@@ -0,0 +1,20 @@
+package iavl
+
+import (
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWriteDOTGraph(t *testing.T) {
+	tree, err := getTestTree(0)
+	require.NoError(t, err)
+	for _, ikey := range []byte{
+		0x0a, 0x11, 0x2e, 0x32, 0x50, 0x72, 0x99, 0xa1, 0xe4, 0xf7,
+	} {
+		key := []byte{ikey}
+		tree.Set(key, key) //nolint:errcheck
+	}
+	WriteDOTGraph(io.Discard, tree.ImmutableTree, []PathToLeaf{})
+}
